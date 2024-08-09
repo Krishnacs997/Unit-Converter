f03d137f0d9e75ea4189ba8b1926bf0a79047e30
Krishna <Krishna.chaudhary@hapana.com>
2024-08-09 17:05:05 +0530

    First commit

---
+++ app/src/main/java/com/krishnacs/unitconverter/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.krishnacs.unitconverter
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.krishnacs.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnitConverterTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String = "Krishna", modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "Unit converter")
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {
+            //here goes what happens when the text changes
+        })
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Row {
+            val ctx = LocalContext.current
+            Box{
+                Button(onClick = { Toast.makeText(ctx, "Button clicked", Toast.LENGTH_LONG).show() }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Array drop down")
+                }
+
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(text = {Text( "CM")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text( "Inc")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text( "Feet")}, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = {Text( "mm")}, onClick = { /*TODO*/ })
+                }
+            }
+            Spacer(modifier = Modifier.width(12.dp))
+            Box{
+                Button(onClick = { Toast.makeText(ctx, "Button clicked", Toast.LENGTH_LONG).show() }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Array drop down")
+                }
+            }
+
+
+        }
+        Text(text = "Result: ")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnitConverterTheme {
+        Greeting("Android")
+    }
+}
